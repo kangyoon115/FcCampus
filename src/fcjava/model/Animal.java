5144a80f04e994c3e2661b5f4c76d6ae49741058
kang <kang01105@naver.com>
2024-03-20 16:48:12 +0900

    변경

--- src/fcjava/model/Animal.java
+++ src/fcjava/model/Animal.java
@@ -1,10 +1,11 @@
 package fcjava.model;
 
-public class Animal extends Object{
-    public Animal(){
+public class Animal extends Object {
+    public Animal() {
         super(); // new Object
     }
-    public void eat(){
+
+    public void eat() {
         System.out.println("동물처럼 먹다");//추상적인 표현
     }
 }
---
+++ src/part1/ObjectCasting.java
@@ -0,0 +1,17 @@
+package part1;
+
+import fcjava.model.*;
+
+public class ObjectCasting {
+    public static void main(String[] args) {
+        // Animal-> Dog,Cat 상속
+        Animal ani = new Dog();
+        ani.eat();
+
+        ani = new Cat(); //업케스팅
+        ani.eat();
+        //Cat c=(Cat)ani;//다운케스팅:night는 개에 없고 고양에이만 있는 상황
+        //c.night();
+        ((Cat)ani).night(); //로도 가능
+    }
+}
