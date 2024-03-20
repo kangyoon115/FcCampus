7342360080da7e68fcb79a180158fedc5f6252a8
kang <kang01105@naver.com>
2024-03-20 16:53:16 +0900

    변경

---
+++ src/part1/PolyTest.java
@@ -0,0 +1,23 @@
+package part1;
+
+import fcjava.model.Animal;
+import fcjava.model.Dog;
+import fcjava.model.Cat;
+
+public class PolyTest {
+    public static void main(String[] args) {
+        //Upcasting으로 객체를 생성
+        //상속관계, 재정의(Override), 동적바인딩
+        //다형성
+        //상위클래스가 동일한 메세지를 보내면 하위클래스를 서로 다르게 동작시키는 객체지향
+        Animal ani = new Dog();
+        ani.eat();
+
+        //Donwcasting 다운케스팅
+        ani= new Cat();
+        ani.eat();
+        ((Cat).ani).night();
+
+    }
+
+}
