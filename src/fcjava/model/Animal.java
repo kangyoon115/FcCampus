2dfa321347e722e04878b4e05f16cc19673bb1f8
kang <kang01105@naver.com>
2024-03-20 11:36:57 +0900

    변경

--- src/fcjava/model/Animal.java
+++ src/fcjava/model/Animal.java
@@ -3,6 +3,5 @@ package fcjava.model;
 public class Animal {
     public void eat(){
         System.out.println("동물처럼 먹다");//추상적인 표현
-
     }
 }
---
+++ src/part1/DogCatUpcastingTest.java
@@ -0,0 +1,20 @@
+package part1;
+
+import fcjava.model.Animal;
+import fcjava.model.Cat;
+import fcjava.model.Dog;
+
+public class DogCatUpcastingTest {
+    public static void main(String[] args)
+    {
+        //Dog 객체를 사용해보자
+        //Upcasting(업케스팅)
+        //부모가 자식을 가리킨다.
+        Animal ani = new Dog();
+        ani.eat(); // 동물처럼 먹다 -> 개처럼 먹다
+
+        ani = new Cat();
+        ani.eat(); // 동물처럼 먹다 -> 고양이처럼 먹다
+
+    }
+}
