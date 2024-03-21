593877d5199e049054baac744922e8861c34707e
kang <kang01105@naver.com>
2024-03-22 00:07:46 +0900

    인터페이스 실습

--- src/part1/InterfaceTest.java
+++ src/part1/InterfaceTest.java
@@ -7,6 +7,8 @@ public class InterfaceTest {
         //리모콘으로 Radio와 TV를 동작시켜보자.
         //다형성이 100%보장된다.
         //부모가 인터페이스이면 자식의 내부 동작방식을 전혀 몰라도 동작을 시킬수가 있다.
+        //RemoCon r=new RemoCon(); 객체생성불가
+        //부모의 역할을 할 수 있다.
         RemoCon remo=new Radio();
         remo.chUp();
         remo.chDown();
