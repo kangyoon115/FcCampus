6f8d2f0c29eb21c22ec0e4d49887db9f6d0f468c
kang <kang01105@naver.com>
2024-03-21 16:17:38 +0900

    추상클래스

---
+++ src/part1/AbstractClassTest.java
@@ -0,0 +1,22 @@
+package part1;
+
+import poly.Animal;
+import poly.Cat;
+import poly.Dog;
+
+public class AbstractClassTest {
+    public static void main(String[] args) {
+        //추상클래스는 단독으로 객체를 생성할 수 없다.
+        //Animal ani = new Animal(); //객체를 생성할 수 없다. 왜? 불완전해서
+        //ani.eat();
+        //추상클래스는 부모의 역할은 할 수 있다.
+        Animal ani = new Dog();
+        ani.eat();//개처럼 먹다
+        ani.move();
+
+        ani = new Cat();
+        ani.eat();//고양이처럼 먹다
+        ani.move();
+        ((Cat)ani).night();
+    }
+}
--- src/poly/Animal.java
+++ src/poly/Animal.java
@@ -1,7 +1,12 @@
 package poly;
 //추상클래스
 public abstract class Animal {
+    //추상메서드
     public void eat(){
 
     }; //추상메서드(불완전한 메서드): 메서드의 구현부가 없다.
+    //구현메서드
+    public void move(){
+        System.out.println("무리를 지어서 이동한다");
+    }
 }
