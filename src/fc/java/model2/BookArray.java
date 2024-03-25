6e5f9a7c042cbdb4caaad97ef58e328a748213e4
kang <kang01105@naver.com>
2024-03-25 16:37:42 +0900

    책 배열 API

---
+++ src/fc/java/model2/BookArray.java
@@ -0,0 +1,40 @@
+package fc.java.model2;
+
+import java.util.Arrays;
+
+public class BookArray { //배열처럼 동작하는 클래스
+    private final int DEFAULT_CAPACITY=5;//final=절대로 이 값을 수정할 수 없음->상수
+    private Book[] elements;
+    private int size=0;
+    //생성동작
+    public BookArray(){
+        elements= new Book[DEFAULT_CAPACITY];//5개 크기배열
+    }
+    //저장하는 동작
+    public void add(Book element){
+        //크기체크? if
+        if(size==elements.length){
+            //크기를 2배로 늘리는 작업
+            ensureCapacity();
+        }
+        elements[size++]=element;
+    }
+
+
+    // 얻는동작
+    public Book get(int index){//-1,5~
+        //index체크? if
+        if(index<0||index>=size){
+            throw new IndexOutOfBoundsException("index의 범위(0~4)가 초과됨");//JVM이 처리
+        }
+        return elements[index];
+    }
+    //원소의 개수를 넘겨주는 동작
+    public int size(){
+        return size;
+    }
+    private void ensureCapacity() {
+        int newCapacity=elements.length*2;
+        elements=Arrays.copyOf(elements,newCapacity);
+    }
+}
